45272f8fc19830eae2c2e2f2e62fdd7bbe4f2596
jiaji <jiajiz@andrew.cmu.edu>
2017-09-30 14:52:37 -0400

    added singleton

---
+++ include/yifanlibrary/SINGLETON.h
@@ -0,0 +1,23 @@
+#ifndef _SINGLETON_CLASS_HEADER_
+#define _SINGLETON_CLASS_HEADER_
+
+class SINGLETON
+{
+public:
+	/// for singleton implementation 
+	static SINGLETON* Instance();
+
+protected: 
+	/// for singleton implementation 
+	static SINGLETON* pinstance; 
+	/// for singleton implementation 
+	SINGLETON(); 
+	/// for singleton implementation 
+	SINGLETON(const SINGLETON&); 
+	/// for singleton implementation 
+	SINGLETON& operator= (const SINGLETON&); 
+	~SINGLETON(); 
+
+};
+
+#endif
---
+++ src/SINGLETON.cpp
@@ -0,0 +1,30 @@
+#include "SINGLETON.h"
+
+SINGLETON* SINGLETON::pinstance = 0;
+
+
+SINGLETON* SINGLETON::Instance()
+{
+	if (pinstance == 0) // first time call
+	{
+		pinstance = new SINGLETON;
+	}
+	return pinstance;
+}
+
+SINGLETON::SINGLETON()
+{
+
+}
+
+SINGLETON & SINGLETON::operator=(const SINGLETON & olc)
+{
+	// shall never get called
+	SINGLETON* lc = new SINGLETON();
+	return *lc;
+}
+
+SINGLETON::~SINGLETON()
+{
+	delete pinstance;
+}
